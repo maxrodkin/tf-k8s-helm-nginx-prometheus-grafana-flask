52df4270f67e299044e0ba8c6372d57d307b4d3c
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-07 11:04:31 +0000

    added ingress

--- prometheus-grafana-install.sh
+++ prometheus-grafana-install.sh
@@ -4,13 +4,3 @@ helm upgrade ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ing
 
 kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
 kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
-
-sudo docker pull calinrus/hit-counter
-sudo docker pull redis:alpine
-
-kubectl run redis-lb --image=redis:alpine --port=6379
-
-redis_ip=$(kubectl get po -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r")
-echo $redis_ip
-sed -r "s/redis_ip/$redis_ip/" flask.yaml | kubectl apply -f -
-
