7ba68e8384cb84302d123bf494dcd110220f8cfd
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-07 08:30:05 +0000

    tested flask

--- nginx-prometheus-grafana-flask-install.sh
+++ nginx-prometheus-grafana-flask-install.sh
@@ -9,4 +9,8 @@ sudo docker pull calinrus/hit-counter
 sudo docker pull redis:alpine
 
 kubectl run redis-lb --image=redis:alpine --port=6379
-kubectl run flask --image=calinrus/hit-counter --port=5000
+
+redis_ip=$(kubectl get po -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r")
+echo $redis_ip
+sed -r "s/redis_ip/$redis_ip/" flask.yaml | kubectl apply -f -
+
