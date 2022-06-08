f2ba85aa09d7e2afa7f85cee1d6fc46b8f08c03c
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-08 10:28:00 +0000

    have got ingress ip

--- prometheus-grafana-install.sh
+++ prometheus-grafana-install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace
-helm upgrade ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --set controller.metrics.enabled=true --set-string controller.podAnnotations."prometheus\.io/scrape"="true" --set-string controller.podAnnotations."prometheus\.io/port"="10254"
-
 kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
 kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
+kubectl apply -f nginx_ingress-prometheus-grafana-flask.yaml
+#and find the ingress ip:
+#$ kubectl get svc -n ingress-nginx |grep ingress-nginx-controller
+#ingress-nginx-controller             LoadBalancer   10.108.205.167   <pending>     80:32191/TCP,443:30735/TCP   117m
+#curl http://10.108.205.167/health
+#curl http://10.108.205.167/grafana
+#curl http://10.108.205.167:32191/app
