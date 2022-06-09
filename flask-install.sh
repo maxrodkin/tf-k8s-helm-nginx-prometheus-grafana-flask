d9d06168de42fc6f2797d677016e8510eb3aa0e3
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-09 10:00:10 +0000

    minor fix

--- flask-install.sh
+++ flask-install.sh
@@ -5,7 +5,6 @@ sudo docker pull redis:alpine
 
 kubectl run redis-lb --image=redis:alpine --port=6379 --namespace=ingress-nginx
 
-redis_ip=$(kubectl get po --namespace=ingress-nginx -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r")
-echo $redis_ip
+redis_ip=$(kubectl get po --namespace=ingress-nginx -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r") && echo $redis_ip
 sed -r "s/redis_ip/$redis_ip/" flask.yaml | kubectl apply -f -
 
