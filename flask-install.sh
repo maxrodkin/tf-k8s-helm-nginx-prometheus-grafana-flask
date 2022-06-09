188f4c3af5edc021ebb02505e21dceac8be4270a
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-09 10:15:36 +0000

    forgot script flask service, fixing

--- flask-install.sh
+++ flask-install.sh
@@ -8,3 +8,5 @@ kubectl run redis-lb --image=redis:alpine --port=6379 --namespace=ingress-nginx
 redis_ip=$(kubectl get po --namespace=ingress-nginx -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r") && echo $redis_ip
 sed -r "s/redis_ip/$redis_ip/" flask.yaml | kubectl apply -f -
 
+ kubectl apply -f flask-service.yaml
+
