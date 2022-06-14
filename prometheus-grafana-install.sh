f528306fdbdf8afb40704b0ad41f54e5b3a52a89
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-14 09:04:56 +0000

    minor

--- prometheus-grafana-install.sh
+++ prometheus-grafana-install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
+#ingress controller
 helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace
-#kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
+#prometheus
 kubectl apply --kustomize github.com/maxrodkin/ingress-nginx/deploy/prometheus/
-#kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
 
 public_hostname=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname|tr -d " \t\n\r") && echo $public_hostname
+
+#grafana
 sed -r "s/;domain = localhost/domain = $public_hostname/" grafana.ini > grafana.ini.updated
 sed -ri "s/;root_url/root_url/" grafana.ini.updated 
 sed -ri "s/;serve_from_sub_path = false/serve_from_sub_path = true/" grafana.ini.updated 
 kubectl create configmap grafana.ini -n ingress-nginx --from-file=./grafana.ini.updated
-kubectl apply --kustomize github.com/maxrodkin/ingress-nginx/deploy/grafana/
+kubectl apply --kustomize github.com/maxrodkin/ingress-nginx/deploy/grafana/ 
+
+#ingress
 kubectl apply -f nginx_ingress-prometheus-grafana-flask.yaml
-#and find the ingress ip:
-#$ kubectl get svc -n ingress-nginx |grep ingress-nginx-controller
-#ingress-nginx-controller             LoadBalancer   10.108.205.167   <pending>     80:32191/TCP,443:30735/TCP   117m
-#curl http://10.108.205.167/health
-#curl http://10.108.205.167/grafana
-#curl http://10.108.205.167:32191/app
+
