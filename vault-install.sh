5e0dd613021af004e1e458ae8eeed102fabd96b9
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-15 15:47:58 +0000

    vault installed

--- vault-install.sh
+++ vault-install.sh
@@ -6,5 +6,17 @@ sed -r "s/name: data-vault/name: data-vault-2/" data-vault.yaml | kubectl apply 
 
 kubectl create namespace vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
+helm install vault hashicorp/vault -n vault  --values helm-vault-raft-values.yml
 #saddly no way to decrease pvc demand
 #kubectl patch pvc data-vault-0 -n vault -p '{"spec":{"resources":{"requests":{"storage":"1Gi"}}}}'
+#saddly no way to edit pot except his image
+#kubectl patch po vault-0 -n vault -p '{"spec":{"affinity":{"podAntiAffinity": null}}}'
+kubectl exec vault-0 -n vault -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-keys.json
+VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" cluster-keys.json)
+kubectl exec vault-0 -n vault -- vault operator unseal $VAULT_UNSEAL_KEY
+jq -r ".root_token" cluster-keys.json
+#kubectl exec --stdin=true --tty=true vault-0 -n vault -- sh
+#vault login
+#vault kv put secret/grafana username="admin" password="P@ssw0rd_1"
+#vault kv get secret/grafana
+#exit
