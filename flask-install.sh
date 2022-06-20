b5f08e4efff24352bf5e39f530c265a35fc4d4f8
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-20 13:07:38 +0000

    minor

--- flask-install.sh
+++ flask-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 kubectl create ns ingress-nginx
-sleep 10
 sudo docker pull maxi4/hit-counter:prometheus_metrics
 sudo docker pull redis:alpine
 
 kubectl run redis-lb --image=redis:alpine --port=6379 --namespace=ingress-nginx
+sleep 10
 
 redis_ip=$(kubectl get po --namespace=ingress-nginx -o wide|grep redis-lb|awk '{ print $6}'|tr -d " \t\n\r") && echo $redis_ip
 sed -r "s/redis_ip/$redis_ip/" flask.yaml | kubectl apply -f -
