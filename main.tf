177f6fa42ee5f5931599930af443ebed09b00c71
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-20 10:02:17 +0000

    tf works

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "454802319560_Tours-Admin-UserRole"
+  region = var.region
+}
+
+resource "aws_instance" "k8s" {
+  ami = var.ami
+  instance_type = "t2.medium"
+#  vpc_id = var.vpc_id
+  vpc_security_group_ids = [var.security_group_id]
+  subnet_id = var.subnet_id
+  key_name = var.key_name
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="maxrodkin_jumpbox_2"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+}
+
+
+output "ec2instance" {
+  value = aws_instance.k8s.public_dns
+  }
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "vpc_id" {type = string}
+variable "subnet_id" {type = string}
+variable "security_group_id" {type = string}
+variable "ami" {type = string}
+variable "key_name" {type = string}
+variable "region" {type = string}
