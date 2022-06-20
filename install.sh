5cb110c0385618e2ad56e7718ff6a20a1a0a6616
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-20 14:13:49 +0000

    minor

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git
-#cd tf-k8s-helm-nginx-prometheus-grafana-flask
-#./docker_install.sh  
-#./get_helm.sh      
-#./k8s_install.sh                 
-#./flask-install.sh   
-#./helm_install.sh  
-#./prometheus-grafana-install.sh  
-./vault-install.sh
+git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git
+cd tf-k8s-helm-nginx-prometheus-grafana-flask
+./docker_install.sh  
+./get_helm.sh      
+./k8s_install.sh                 
+./flask-install.sh   
+./helm_install.sh  
+./prometheus-grafana-install.sh  
+#./vault-install.sh
