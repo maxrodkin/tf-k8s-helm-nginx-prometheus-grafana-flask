67fa0677e32f25cec216129b679edfaafd1dcf35
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-21 13:13:38 +0000

    fix

--- main.tf
+++ main.tf
@@ -30,7 +30,18 @@ resource "aws_instance" "k8s" {
     OS = "UBUNTU"
     Managed = "IAC"
   }
+  user_data = << EOF
+#!/bin/bash
+git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git \
+&& cd tf-k8s-helm-nginx-prometheus-grafana-flask
+./docker_install.sh  
+./get_helm.sh      
+./k8s_install.sh                 
+./flask-install.sh   
+./prometheus-grafana-install.sh  
+EOF
 
+}
 }
 
 
