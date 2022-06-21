f07b48e4c7e2b5abdf96e67b8dc5b83362ba69b9
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-21 13:07:32 +0000

    fix

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git
-cd tf-k8s-helm-nginx-prometheus-grafana-flask
+git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git \
+&& cd tf-k8s-helm-nginx-prometheus-grafana-flask
 ./docker_install.sh  
 #./helm_install.sh 
 ./get_helm.sh      
 ./k8s_install.sh                 
 ./flask-install.sh   
- 
 ./prometheus-grafana-install.sh  
-./vault-install.sh
+#./vault-install.sh
