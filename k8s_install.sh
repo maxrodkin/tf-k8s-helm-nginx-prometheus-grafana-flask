197f74f52327d00fc4973d54c0a79bd5a6667f1c
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-06-21 16:38:52 +0000

    fix

--- k8s_install.sh
+++ k8s_install.sh
@@ -34,6 +34,7 @@ sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube && \
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml && \
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
