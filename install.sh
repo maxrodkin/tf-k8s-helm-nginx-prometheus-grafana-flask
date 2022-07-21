a801bd3208bccc31dcf3fb17f4207fc536569ecd
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-07-21 14:47:34 +0000

    debug ingress path issue with standalone nginx

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git \
 && cd tf-k8s-helm-nginx-prometheus-grafana-flask
 ./docker_install.sh  
-#./helm_install.sh 
 ./get_helm.sh      
 ./k8s_install.sh                 
 ./flask-install.sh   
-./prometheus-grafana-install.sh  
-#./vault-install.sh
+./prometheus-grafana-install.sh
 
 cd nginx-standalone.conf
 ./nginx-standalone-install.sh
