3af0a240a2d857fab6c8f1bde4ee23953d333624
MaxRodkin <ext.maxim.rodkin@ef.com>
2022-07-21 14:35:26 +0000

    debug ingress path issue with standalone nginx

--- main.tf
+++ main.tf
@@ -34,11 +34,7 @@ resource "aws_instance" "k8s" {
 #!/bin/bash
 git clone https://github.com/maxrodkin/tf-k8s-helm-nginx-prometheus-grafana-flask.git \
 && cd tf-k8s-helm-nginx-prometheus-grafana-flask
-./docker_install.sh  
-./get_helm.sh      
-./k8s_install.sh                 
-./flask-install.sh   
-./prometheus-grafana-install.sh
+./install.sh  
 
 EOF
 
